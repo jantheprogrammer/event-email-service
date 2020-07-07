ea3bf388bda861e46c75a25f07c1ad7fb9be6e89
Jan Krejca <jantheprogrammer@gmail.com>
2020-07-07 22:20:18 +0200

    unsuccessful resolution of two executions

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from flask_cors import CORS
-import os
 
 from email_service import send_email
-
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-
-if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
-    scheduler = BackgroundScheduler(daemon=True)
-    scheduler.add_job(send_email, 'cron', day_of_week='tue', hour=20, minute=15)  # server time is 2 hours behind Prague time
-    scheduler.start()
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.add_job(send_email, 'cron', day_of_week='mon', hour=6)  # heroku server time is 2 hours behind Prague time
+scheduler.start()
 
 
 @app.route("/send_email", methods=['GET'])
 def send_events_email():
-
     send_email()
 
     return "Email has been sent!"
 
 
 if __name__ == "__main__":
-    app.run(use_reloader=False)
+    app.run()
