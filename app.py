6292fcf7e664fdde0ddc24e39f1ba53af7fa147a
Jan Krejca <jantheprogrammer@gmail.com>
2020-07-07 20:46:22 +0200

    heroku scheduler test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from email_service import send_email
 
 scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(send_email, 'cron', day_of_week='mon', hour=8)
+scheduler.add_job(send_email, 'cron', day_of_week='tue', hour=19)
 scheduler.start()
 
 app = Flask(__name__)
