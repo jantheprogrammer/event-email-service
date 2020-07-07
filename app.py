c174ec75a05da843609fd1752e3ed5f7e2f239e8
Jan Krejca <jantheprogrammer@gmail.com>
2020-07-07 22:11:15 +0200

    debugging the scheduler

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from flask_cors import CORS
+import os
 
 from email_service import send_email
 
-scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(send_email, 'cron', day_of_week='tue', hour=20)  # server time is 2 hours behind Prague time
-scheduler.start()
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+    scheduler = BackgroundScheduler(daemon=True)
+    scheduler.add_job(send_email, 'cron', day_of_week='tue', hour=20, minute=15)  # server time is 2 hours behind Prague time
+    scheduler.start()
 
 
 @app.route("/send_email", methods=['GET'])
@@ -21,4 +25,4 @@ def send_events_email():
 
 
 if __name__ == "__main__":
-    app.run(use_reloader=False)  # use_reloader=False prevent the scheduler from executing twice
+    app.run(use_reloader=False)
